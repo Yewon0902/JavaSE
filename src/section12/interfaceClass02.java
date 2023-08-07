ab82c1ada0324e093f38e266f0f7a2cdbe48a7e1
nww <nww>
2023-08-07 22:20:49 +0900

    2023_08_07

--- src/section12/interfaceClass02.java
+++ src/section12/interfaceClass02.java
@@ -1,20 +1,49 @@
 package section12;
+
+import section12.acces3.BluetoothMIC;
+import section12.acces3.Microphone;
+import section12.acces3.Speaker;
+import section12.acces3.TJmic;
+
 /*
  * 인터페이스 상속
- *  인터페이스끼리 상속관계를 만들수 있다.
- *  클래스 상속과 마찬가지로 extends 키워드 사용한다.
- *  다중 상속이 가능하다. 콤마(,) 사용
+ * 	인터페이스끼리 상속관계를 만들수 있다.
+ * 	클래스 상속과 마찬가지로 extends 키워드 사용한다.
+ * 	다중 상속이 가능하다. 콤마(,) 사용
  * 
  */
 public class interfaceClass02 {
-
-	 public static <TJmic> void main(String[] args) {
-		 System.out.println("---TJmic 객체 ---");
-		 TJmic tj = new TJmic();
-		 tj.connect();
-		 tj.music();
-		 tj.sing();
-		 
-		 
-	 }
+	
+	public static void main(String[] args) {
+		System.out.println("---TJmic 객체---");
+		TJmic tj = new TJmic();
+		tj.connect();
+		tj.music();
+		tj.sing();
+		
+		System.out.println("---BluetoothMIC로 타입 변환---");
+		BluetoothMIC bm = tj;
+		bm.connect();
+		bm.music();
+		bm.sing();
+		
+		System.out.println("---Microphone으로 타입 변환---");
+		Microphone m = tj;
+		// m.connect(); 
+		// m.music();
+		m.sing();
+		
+		System.out.println("---Speaker로 타입 변환---");
+		Speaker s = tj;
+		// s.connect();
+		s.music();
+		// s.sing();
+	}
+	
 }
+
+
+
+
+
+
